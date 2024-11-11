961132ac6a6257093b105c19ab3cd23a295ef46a
murdex5 <malshapraboth@Malshas-Laptop>
2024-11-11 22:10:38 +0100

    added the core functionality

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -9,4 +9,51 @@
 /*   Updated: 2024/11/11 21:31:26 by kadferna      ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+char    *ft_strcpy (char *dest, char *src)
+{
+    int i;
 
+    i = 0;
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+}
+/*
+int get_length(char *str)
+{
+    int len;
+
+    while (str[len] != '\0')
+    {
+        len++;
+    }
+
+    return len;
+}
+
+int main(void)
+{
+
+    char *gg = "Hello, World!";
+    // Allocate enough memory for 'hello' to hold 'gg' and the null terminator
+    char *hello = malloc((strlen(gg) + 1) * sizeof(char));
+
+    if (hello == NULL) {
+        // Check if memory allocation failed
+        printf("Memory allocation failed\n");
+        return 1;
+    }
+
+    char *ll = ft_strcpy(hello, gg);
+    printf("%s\n", hello);
+
+    // Free the allocated memory
+    free(hello);
+    return 0;
+} */
