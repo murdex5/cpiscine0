88d7fbd571611577284c3020518da718e8e03ed0
Kadupitige Praboth Malsha Fernando <kadferna@e3r8p6.42roma.it>
2024-11-12 10:17:52 +0100

    added ex00

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -1,59 +1,40 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   ft_strcpy.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kadferna <kadferna@student.42    +#+  +:+       +#+        */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 21:31:24 by kadferna       #+#    #+#             */
-/*   Updated: 2024/11/11 21:31:26 by kadferna      ###   ########.fr       */
+/*   Created: 2024/11/12 10:11:28 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/12 10:14:17 by kadferna         ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
-#include <stdio.h>
-#include <stdlib.h>
-char    *ft_strcpy (char *dest, char *src)
-{
-    int i;
+/* ************************************************************************** */
 
-    i = 0;
-    while (src[i] != '\0')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    dest[i] = '\0';
-    return (dest);
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
+
 /*
-int get_length(char *str)
+int main()
 {
-    int len;
+    char src[] = "Hello, World!";
+    char dest[50];
 
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+    ft_strcpy(dest, src);
 
-    return len;
-}
+    
+    printf("Source string: %s\n", src);
+    printf("Destination string after ft_strcpy: %s\n", dest);
 
-int main(void)
-{
-
-    char *gg = "Hello, World!";
-    // Allocate enough memory for 'hello' to hold 'gg' and the null terminator
-    char *hello = malloc((strlen(gg) + 1) * sizeof(char));
-
-    if (hello == NULL) {
-        // Check if memory allocation failed
-        printf("Memory allocation failed\n");
-        return 1;
-    }
-
-    char *ll = ft_strcpy(hello, gg);
-    printf("%s\n", hello);
-
-    // Free the allocated memory
-    free(hello);
     return 0;
-} */
+}*/
