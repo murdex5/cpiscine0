d950e7d427d59387df914dd99a277cb645924ee2
Kadupitige Praboth Malsha Fernando <kadferna@e3r8p6.42roma.it>
2024-11-12 10:36:35 +0100

    added strncpy.c

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 10:18:47 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/12 10:35:36 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+/*
+int main() {
+    char src[] = "Hello";
+    char dest[50];
+
+    ft_strncpy(dest, src, 10);
+
+   
+    printf("Source: %s\n", src);
+    printf("Destination: %s\n", dest);
+
+    return 0;
+}*/
