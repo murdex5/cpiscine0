5f564b207c7d02c58bef99e904af844678abf12a
Kadupitige Praboth Malsha Fernando <kadferna@e3r8p8.42roma.it>
2024-11-12 17:57:56 +0100

    added ex03

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 17:07:38 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/12 17:57:13 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+	int	l;
+	int	c;
+
+	l = 0;
+	c = 0;
+	while (str[c] != '\0' && l == 0)
+	{
+		if (str[c] >= '0' && str[c] <= '9')
+		{
+			i = 1;
+		}
+		else
+		{
+			i = 0;
+			l = 1;
+		}
+		c++;
+	}
+	if (str[0] == '\0')
+	{
+		i = 0;
+	}
+	return (i);
+}
+
+/*
+int main(int argc, char *argv[])
+{
+    if  (argc != 2)
+    {
+        printf("Invalid inputs");
+        return 1;
+    }
+    else
+    {
+        int i = ft_str_is_numeric(argv[1]);
+        printf("%d\n", i);
+    }
+
+    return 0;
+}*/
