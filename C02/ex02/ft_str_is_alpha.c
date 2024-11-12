1806e33deaa1371f04a2ddd4e10d5d0320c4145e
Kadupitige Praboth Malsha Fernando <kadferna@e3r8p6.42roma.it>
2024-11-12 12:20:47 +0100

    added ft_str is alpha

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_alpha.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 10:39:45 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/12 12:17:30 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+	int	c;
+
+	c = 0;
+	while (str[c] != '\0')
+	{	
+		i = 0;
+		if ((str[c] >= 'A' && str[c] <= 'Z') \
+			|| (str[c] >= 'a' && str[c] <= 'z'))
+		{
+			i = 1;
+		}
+		else
+		{
+			i = 0;
+		}
+		c++;
+	}
+	if (str[0] == '\0')
+	{
+		i = 1;
+	}
+	return (i);
+}
+
+int main(int argc, char **argv)
+{
+	int	i;
+	if (argc != 2)
+	{
+		printf("Invalind input\n");
+	}
+	else
+	{
+		i = ft_str_is_alpha(argv[1]);
+		printf("%d\n", i);
+	}	return 0;
+}
