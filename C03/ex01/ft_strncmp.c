fbea2b720e8f6c72e7437346b422395c230c507b
Kadupitige Praboth Malsha Fernando <kadferna@e3r8p8.42roma.it>
2024-11-14 13:25:28 +0100

    added ex02

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 12:14:04 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/14 12:55:39 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n && (s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
+/*
+int main() 
+{
+	printf("[1] Compare identical strings with n > length: ");
+	printf("Expected: 0, Got: %d\n", ft_strncmp("hello", "hello", 10));
+
+	return 0;
+}*/
---
+++ C03/ex02/ft_strcat.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/14 12:57:18 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/14 13:25:10 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	l;
+
+	i = 0;
+	l = 0;
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+	if (src[0] == '\0')
+	{
+		return (dest);
+	}
+	while (src[i] != '\0')
+	{
+		dest[l + i] = src[i];
+		i++;
+	}
+	dest[l + i] = '\0';
+	return (dest);
+}
+/*
+int main(void)
+{
+    // Test 1: Basic concatenation
+    char dest1[50] = "Hello ";
+    char src1[] = "World!";
+    printf("Test 1 - Basic concatenation:\n");
+    printf("Before: dest = \"%s\", src = \"%s\"\n", dest1, src1);
+    ft_strcat(dest1, src1);
+    printf("After:  dest = \"%s\"\n", dest1);
+    printf("Expected result: \"Hello World!\"\n\n");
+
+    // Test 2: Empty destination string
+    char dest2[50] = "";
+    char src2[] = "Test";
+    printf("Test 2 - Empty destination:\n");
+    printf("Before: dest = \"%s\", src = \"%s\"\n", dest2, src2);
+    ft_strcat(dest2, src2);
+    printf("After:  dest = \"%s\"\n", dest2);
+    printf("Expected result: \"Test\"\n\n");
+
+    // Test 3: Empty source string
+    char dest3[50] = "Hello";
+    char src3[] = "";
+    printf("Test 3 - Empty source:\n");
+    printf("Before: dest = \"%s\", src = \"%s\"\n", dest3, src3);
+    ft_strcat(dest3, src3);
+    printf("After:  dest = \"%s\"\n", dest3);
+    printf("Expected result: \"Hello\"\n\n");
+
+    // Test 4: Multiple concatenations
+    char dest4[50] = "One";
+    char src4_1[] = " Two";
+    char src4_2[] = " Three";
+    printf("Test 4 - Multiple concatenations:\n");
+    printf("Before: dest = \"%s\"\n", dest4);
+    ft_strcat(dest4, src4_1);
+    ft_strcat(dest4, src4_2);
+    printf("After:  dest = \"%s\"\n", dest4);
+    printf("Expected result: \"One Two Three\"\n\n");
+
+    // Test 5: Special characters
+    char dest5[50] = "Hello\t";
+    char src5[] = "World\n";
+    printf("Test 5 - Special characters:\n");
+    printf("Before: dest = \"%s\", src = \"%s\"\n", dest5, src5);
+    ft_strcat(dest5, src5);
+    printf("After:  dest = \"%s\"\n", dest5);
+    printf("Expected result: \"Hello\\tWorld\\n\"\n\n");
+
+    return (0);
+}*/
