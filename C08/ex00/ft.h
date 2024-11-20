7c48f70a9fc4bb0846d53acba6f14d7a0c0d8b78
Kadupitige Praboth Malsha Fernando <kadferna@e3r7p8.42roma.it>
2024-11-20 20:02:03 +0100

    hell

---
+++ C08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 19:21:49 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/20 19:59:29 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_H
+# define FT_H
+#include <unistd.h>
+
+#define	void	ft_putchar(char c);
+#define void	ft_swap(int *a, int *b);
+#define void	ft_putstr(char *str);
+#define int	ft_strlen(char *str);
+#define	int	ft_strcmp(char *s1, char *s2);
+#endif
---
+++ C08/ex01/ft_boolean.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/20 19:34:20 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/20 19:53:08 by kadferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef FT_BOOLEAN
+# define FT_BOOLEAN
+#include <unistd.h>
+
+#define TRUE 1
+#define FALSE 0
+#define EVEN(x) !(x % 2)
+#define EVEN_MSG "I have an even number of arguments\n"
+#define ODD_MSG "I have an odd number of arguments\n"
+#define SUCCESS 0
+
+typedef	int	t_bool;
+#endif
