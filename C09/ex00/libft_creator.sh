7bffeb9cf675b6320e7671d23ae3879c2128e865
Kadupitige Praboth Malsha Fernando <kadferna@e3r7p8.42roma.it>
2024-11-21 17:31:20 +0100

    modified libfit_creator.sh

--- C09/ex00/libft_creator.sh
+++ C09/ex00/libft_creator.sh
@@ -6,10 +6,10 @@
 #    By: kadferna <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/11/21 16:43:34 by kadferna          #+#    #+#              #
-#    Updated: 2024/11/21 17:12:23 by kadferna         ###   ########.fr        #
+#    Updated: 2024/11/21 17:29:57 by kadferna         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 #!/usr/bim/env bash
 
 gcc -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
-ar rcs libfit.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
+ar rcs libft.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
