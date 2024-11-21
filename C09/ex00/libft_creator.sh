84d95edf201f10cb271ecd2e06178e09aea2636a
Kadupitige Praboth Malsha Fernando <kadferna@e3r7p8.42roma.it>
2024-11-21 17:14:59 +0100

    added

---
+++ C09/ex00/libft_creator.sh
@@ -0,0 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    libft_creator.sh                                   :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: kadferna <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/11/21 16:43:34 by kadferna          #+#    #+#              #
+#    Updated: 2024/11/21 17:12:23 by kadferna         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+#!/usr/bim/env bash
+
+gcc -c ft_putchar.c ft_swap.c ft_putstr.c ft_strlen.c ft_strcmp.c
+ar rcs libfit.a ft_putchar.o ft_swap.o ft_putstr.o ft_strlen.o ft_strcmp.o
