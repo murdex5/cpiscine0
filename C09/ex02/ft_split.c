aea6da859ec44f7454847de6b2b620710707b3c9
Kadupitige Praboth Malsha Fernando <kadferna@e3r7p8.42roma.it>
2024-11-23 19:53:42 +0100

    dkf

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -5,124 +5,129 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kadferna <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/23 14:35:18 by kadferna          #+#    #+#             */
-/*   Updated: 2024/11/23 16:02:25 by kadferna         ###   ########.fr       */
+/*   Created: 2024/11/23 18:32:14 by kadferna          #+#    #+#             */
+/*   Updated: 2024/11/23 19:47:00 by kadferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int	ft_strlen(char *str)
+char	*ft_getstr(int start, int end, char *str)
 {
 	int	i;
-	
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-char	*get_string(int start, int end, char *str)
-{
 	int	len;
 	char	*temp;
-	int	i;
 
-	len =  end - start;
-	i = 0;
+	len = end - start;
 	temp = (char *)malloc(len + 1);
 	if (!temp)
 		return (NULL);
-	while (start < end)
+	i = 0;
+	while (i < len)
 	{
-		temp[i] = str[start];
-		start++;
+		temp[i] = str[start + i];
+		i++;
 	}
+	temp[i] = '\0';
 	return (temp);
 }
 
-void	ft_alloc_int_mem(int **arr, char *str, char *charset)
+int	ft_get_mem(char *str, char *charset)
 {
-	int	i;
-	int	c;
 	int	l;
-
-	while (str[i] != '\0')
-	{
-		c = 0;
-		while (charset[c] != '\0')
-		{
-			c = 0;
-			if (str[i] == charset[c])
-				l++;
-			c++;
-		}
-		i++;
-	}
-	arr = (int *)malloc(l * sizeof(int));
-	if (!arr)
-		arr = NULL;
-}
-
-void	ft_alloc_str_mem(int **arr, char **strarr, char *str, char *charset)
-{
-	int	i;
 	int	c;
-	int	len;
-	int	l;
-	int	start;
+	int	i;
+	int	flag;
 
+	if (!(str) || !(charset))
+		return (0);
 	i = 0;
+	flag = 0;
 	l = 0;
-	start = 0;
-	while (!str[i])
+	while (str[i] != '0')
 	{
 		c = 0;
-		while (!charset[c])
+		while (charset[c] != '\0' && flag == 0)
 		{
 			if (str[i] == charset[c])
 			{
-				if (l < 1)
-				{
-					len = ft_strlen(ft_getstring(str[0], str[i]));
-				}
-				else
-				{
-					len = ft_strlen(ft_getstring(str[start], str[i]));
-				}
-				start = i;
 				l++;
+				flag = 1;
 			}
+			break;
 			c++;
 		}
+		flag = 0;
 		i++;
 	}
+	return (l);
 }
+
+void	join_indexes(char **strarr, char *str, char *charset, int len)
+{
+	int	i;
+	int	c;
+	//int	l;
+	int	j;
+	int	start;
+
+	i = 0;
+	//l = 0;
+	c = 0;
+	start = 0;
+	if (!(strarr) || !(str) || !(charset))
+		strarr = NULL;
+	while (str[c] != '\0' && i < len)
+	{
+		j = 0;
+		while (charset[j] != '\0')
+		{
+			//printf("he\n");
+			if (str[c] == charset[j])
+			{
+				strarr[i] = ft_getstr(start, c, str);
+				start = c+1;
+				i++;
+			//	printf("shit happens\n");
+			}
+			break;
+			j++;
+		}
+		c++;
+	}
+}
+
 char	**ft_split(char *str, char *charset)
 {
-	int	**intarr;
 	char	**strarr;
+	int	len;
 
-	intarr = ft_alloc_int_mem(intarr, str, charset);
-
+	len = ft_get_mem(str, charset);
+	strarr = (char **)malloc(len + 1);
+	join_indexes(strarr, str, charset, len);
+	return (strarr);
 }
 
 int	main(void)
 {
-	int	*str;
-	int	**str_arr;
-	str = "Hello world, how are you?";
-	str_arr = ft_split(str, " ,?");
+	char	*str = "Hello, World, how are you?";
+	char	**strarr;
+
+	strarr = ft_split(str, " ,?");
+
+	int l= 0;
+	for (int i = 0; strarr[i]; i++)
+	{
+		 printf("%s\n", strarr[i]);
+		 l++;
+	}
 
 	int i = 0;
-	while (str_arr[i] != NULL)
+	while (i < l)
 	{
-		//int l = 0;
-		printf("%s\n", str_arr[i]);
+		free(strarr[i]);
 		i++;
 	}
-	free(str_arr);
+	free(strarr);
 	return 0;
 }
