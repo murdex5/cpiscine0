584b53c55f3cbb772a06605e70ce64237af09ef8
murdex5 <malshapraboth@Malshas-Laptop>
2024-11-29 00:21:06 +0100

    je

---
+++ Final_Exam/rotone/rotone.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void    rotone(char *str)
+{
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if ((str[i] >= 'A' && str[i] <= 'Y') || (str[i] >= 'a' && str[i] <= 'y'))
+            ft_putchar(str[i] + 1);
+        else if (str[i] == 'Z' || str[i] == 'z')
+            ft_putchar(str[i] - 25);
+        else
+            ft_putchar(str[i]);
+        i++;
+    }
+}
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        write(1, "\n", 1);
+    else
+    {
+        char *str = av[1];
+        rotone(str);
+        ft_putchar('\n');
+    }
+    return 0;
+}
